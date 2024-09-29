4fc93ce364391b45e182f96507faeff81cc266c2
Jeff Tian <jeff.tian@outlook.com>
2024-09-29 12:15:09 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const {ApolloServer} = require('apollo-server');
+const {ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource} = require('@apollo/gateway');
+const {RemoteTransformGraphQLDataSource} = require('@jeff-tian/graphql-transform-federation');
+
+const gateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+        subgraphs: [
+            {name: 'user-service', url: 'https://brickpets-user-service-latest-3q8s.onrender.com/graphql'},
+            {name: 'cms', url: 'https://strapi.brickverse.dev/graphql'},
+        ],
+    }),
+    buildService: ({name, url}) => {
+        if(name === 'cms') {
+            return new RemoteTransformGraphQLDataSource({url, willSendRequest: ({request, context}) => {
+                request.http.headers.set('Authorization', `Bearer ${process.env.CMS_TOKEN}`);
+            }});
+        }
+
+        return new RemoteGraphQLDataSource({url});
+    },
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+    gateway,
+});
+
+server.listen().then(({url}) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
