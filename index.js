826efd14d728df96f141e40b4dae2793d4e918cd
Jeff Tian <jeff.tian@outlook.com>
2024-09-29 22:34:37 +0800

    feat: successfully call cms graphql api

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } = require('@apollo/gateway');
-const { RemoteTransformGraphQLDataSource } = require('@jeff-tian/graphql-transform-federation/dist/remote-transform-graphql-datasource.js');
+const {ApolloServer} = require('apollo-server');
+const {ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource} = require('@apollo/gateway');
+const {RemoteTransformGraphQLDataSource} = require('@jeff-tian/graphql-transform-federation/dist/remote-transform-graphql-datasource.js');
 
 const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
-            { name: 'user-service', url: 'https://brickpets-user-service-latest-3q8s.onrender.com/graphql' },
-            { name: 'cms', url: 'https://strapi.brickverse.dev/graphql' },
+            {name: 'user-service', url: 'https://brickpets-user-service-latest-3q8s.onrender.com/graphql'},
+            {name: 'cms', url: 'https://strapi.brickverse.dev/graphql'},
         ],
     }),
-    buildService: ({ name, url }) => {
+    buildService: ({name, url}) => {
         if (name === 'cms') {
             return new RemoteTransformGraphQLDataSource(
                 url,
-                process.env.CMS_TOKEN
+                {Authorization: `Bearer ${process.env.CMS_TOKEN}`},
             );
         }
 
-        return new RemoteGraphQLDataSource({ url });
+        return new RemoteGraphQLDataSource({url});
     },
 });
 
@@ -26,6 +26,6 @@ const server = new ApolloServer({
     gateway,
 });
 
-server.listen().then(({ url }) => {
+server.listen().then(({url}) => {
     console.log(`🚀 Server ready at ${url}`);
 });
